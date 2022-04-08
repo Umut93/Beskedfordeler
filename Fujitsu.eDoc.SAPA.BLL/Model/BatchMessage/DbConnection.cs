722e07429b5160d86c3325c069d7eb3ff19ff14e
Umut93 <umutkayatuz@live.dk>
2022-04-08 12:54:24 +0200

    Add files via upload

---
+++ Fujitsu.eDoc.SAPA.BLL/Model/BatchMessage/DbConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fujitsu.eDoc.SAPA.BLL.Model
+{
+    public sealed class DbConnection
+    {
+        public static DbConnection instance = null;
+        public string connString = string.Empty;
+
+        public static DbConnection GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DbConnection();
+                }
+                return instance;
+            }
+        }
+
+        public DbConnection()
+        {
+            string connString = System.Configuration.ConfigurationManager.ConnectionStrings["core"].ConnectionString;
+            connString = connString.Replace("net-type=mssql;", "");
+            this.connString = connString;
+        }
+    }
+}
